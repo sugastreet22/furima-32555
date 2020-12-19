c162a54af4e38e986980f2f322b82ab2ecfd3684
sugastreet22 <re22sugar@gmail.com>
2020-12-19 19:45:48 +0900

    userモデルのvaridates

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
+  validates :lastname, presence: true
+  validates :firstname, presence: true
+  validates :lastnamekana, presence: true
+  validates :firstnamekana, presence: true
+  validates :birthday, presence: true
 end

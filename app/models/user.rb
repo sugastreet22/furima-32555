f2d11f85291a024a742e9bea8974966d971f34b3
sugastreet22 <re22sugar@gmail.com>
2020-12-20 15:28:47 +0900

    itemsコントローラー作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :items
   validates :name, presence: true
   validates :lastname, presence: true
   validates :firstname, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'items/index'
   devise_for :users
+  root to: "items#index"
+  resources :items, only: :index
 end

0a0ebfde022a595cfa2479f8a8aa97802e6573c5
sugastreet22 <re22sugar@gmail.com>
2020-12-20 17:30:22 +0900

    新規登録完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :items
-  validates :name, presence: true
-  validates :lastname, presence: true
-  validates :firstname, presence: true
-  validates :lastnamekana, presence: true
-  validates :firstnamekana, presence: true
-  validates :birthday, presence: true
+  # VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  # validates :password, format: { with: VALID_PASSWORD_REGEX }
+  # validates :name, presence: true
+  # validates :lastname, presence: true
+  # validates :firstname, presence: true
+  # validates :lastnamekana, presence: true
+  # validates :firstnamekana, presence: true
+  # validates :birthday, presence: true
 end

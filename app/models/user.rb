d81da7e539f7a22906e9c2fee77d3455a3d7c692
sugastreet22 <re22sugar@gmail.com>
2020-12-20 19:47:16 +0900

    ログイン、ログアウト機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, password_length: 6..128
   has_many :items
-  # VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
-  # validates :password, format: { with: VALID_PASSWORD_REGEX }
-  # validates :name, presence: true
-  # validates :lastname, presence: true
-  # validates :firstname, presence: true
-  # validates :lastnamekana, presence: true
-  # validates :firstnamekana, presence: true
-  # validates :birthday, presence: true
+
+  validates :name, presence: true
+  validates :lastname, presence: true
+  validates :firstname, presence: true
+  validates :lastnamekana, presence: true
+  validates :firstnamekana, presence: true
+  validates :birthday, presence: true
 end

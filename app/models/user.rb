b496623ab17e32e69691a362e133fcd8215c050a
sugastreet22 <re22sugar@gmail.com>
2020-12-21 15:48:45 +0900

    正規表現　半角英数字

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, password_length: 6..128
   has_many :items
-
-  validates :name, presence: true
-  validates :lastname, presence: true
-  validates :firstname, presence: true
-  validates :lastnamekana, presence: true
-  validates :firstnamekana, presence: true
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Password can't be blank"}
+    validates :name, presence: true
+    validates :lastname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Last name can't be blank."}
+    validates :firstname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. First name can't be blank."}
+    validates :lastnamekana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Last name kana can't be blank."}
+    validates :firstnamekana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. First name kana can't be blank."}
+    validates :birthday, presence: true
+  end
 end

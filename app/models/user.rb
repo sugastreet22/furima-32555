09744ca2c13c0ef7ace26a95e8b20a2c638d794f
sugastreet22 <re22sugar@gmail.com>
2020-12-23 15:17:16 +0900

    Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, password_length: 6..128
   has_many :items
   with_options presence: true do
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Password can't be blank"}
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "is invalid. Password can't be blank" }
     validates :name, presence: true
-    validates :lastname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Last name can't be blank."}
-    validates :firstname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. First name can't be blank."}
-    validates :lastnamekana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Last name kana can't be blank."}
-    validates :firstnamekana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. First name kana can't be blank."}
+    validates :lastname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Last name can't be blank." }
+    validates :firstname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. First name can't be blank." }
+    validates :lastnamekana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Last name kana can't be blank." }
+    validates :firstnamekana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. First name kana can't be blank." }
     validates :birthday, presence: true
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :user do
-    name {Faker::Name.last_name}
-    email {Faker::Internet.free_email}
+    name { Faker::Name.last_name }
+    email { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 6)
-    password {password}
-    password_confirmation {password}
-    lastname {"高橋"}
-    firstname {"秀樹"}
-    lastnamekana {"タカハシ"}
-    firstnamekana {"ヒデキ"}
-    birthday  {"2020-02-02"}
+    password { password }
+    password_confirmation { password }
+    lastname { '高橋' }
+    firstname { '秀樹' }
+    lastnamekana { 'タカハシ' }
+    firstnamekana { 'ヒデキ' }
+    birthday { '2020-02-02' }
   end
 end

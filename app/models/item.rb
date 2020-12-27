65b5952cd69cef866851e6b0b773ef97fd4b44f6
sugastreet22 <re22sugar@gmail.com>
2020-12-27 21:15:08 +0900

    バリデーション修正2

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,11 +10,13 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :product
     validates :explain
-    validates :category_id, numericality: { other_than: 1 } 
-    validates :condition_id, numericality: { other_than: 1 } 
-    validates :burden_id, numericality: { other_than: 1 } 
-    validates :area_id, numericality: { other_than: 1 } 
-    validates :day_id, numericality: { other_than: 1 } 
+    with_options numericality: { other_than: 1 } do
+      validates :category_id 
+      validates :condition_id
+      validates :burden_id
+      validates :area_id
+      validates :day_id
+    end
     with_options format: { with: /\A[0-9]+\z/ } do
     validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
     end

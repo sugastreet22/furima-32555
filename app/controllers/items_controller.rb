9f733d924b77e97ed0b66455fd5cbb48e07215c6
sugastreet22 <re22sugar@gmail.com>
2020-12-30 17:44:50 +0900

    ログイン状態の出品者以外のユーザーは、URLを直接入力して出品していない商品の商品情報編集ページへ遷移しようとすると、トップページに遷移すること

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -18,11 +20,10 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
@@ -40,5 +41,11 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:product, :explain, :category_id, :condition_id, :burden_id, :area_id, :day_id, :price, :image).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
+  def move_to_index
+    redirect_to new_user_session_path unless user_signed_in?
+  end
 end

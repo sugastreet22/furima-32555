f6c8f0116571a97e4cd96f960c8d035d8e7380cf
sugastreet22 <re22sugar@gmail.com>
2021-01-05 14:36:49 +0900

    購入内容の確認を表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,16 +2,17 @@ class OrdersController < ApplicationController
 
   def index
     @user_item = UserItem.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
-    @user_item = UserItem.new(item_params)
-    if @user_item.valid?
-      @user_item.save
-      redirect_to action: :index
-    else
-      render action: :new
-    end
+    # @user_item = UserItem.new(item_params)
+    # if @user_item.valid?
+    #   @user_item.save
+    #   redirect_to action: :index
+    # else
+    #   render action: :new
+    # end
   end
 
 end
--- app/models/user_item.rb
+++ app/models/user_item.rb
@@ -5,9 +5,9 @@ class UserItem
   with_options presence: true do
     validates :postal, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :area_id, numericality: { other_than: 1, message: "Prefecture Select" }
-    varidates :city
-    varidates :block
-    varidates :number, format: { with: /\A\d{10,11}\z/, message: "Phone number can't be blank" }
+    validates :city
+    validates :block
+    validates :number, format: { with: /\A\d{10,11}\z/, message: "Phone number can't be blank" }
   end
 
   def save

5b726032ead89289fa347b30ca93eeb84d0f98cc
sugastreet22 <re22sugar@gmail.com>
2021-01-09 19:45:53 +0900

    異常系テストコード追加

--- spec/models/user_item_spec.rb
+++ spec/models/user_item_spec.rb
@@ -11,6 +11,9 @@ RSpec.describe UserItem, type: :model do
       it 'すべての値が正しく入力されていれば保存できること' do
         expect(@user_item).to be_valid
       end
+      it '建物名が空でも購入できること' do
+        expect(@user_item).to be_valid
+      end
     end
 
     context "商品が購入できない場合" do
@@ -39,15 +42,35 @@ RSpec.describe UserItem, type: :model do
         @user_item.valid?
         expect(@user_item.errors.full_messages).to include("Block can't be blank")
       end
-      it 'numberが空だと保存できないこと' do
+      it '電話番号が空だと保存できないこと' do
         @user_item.number = nil
         @user_item.valid?
         expect(@user_item.errors.full_messages).to include("Number can't be blank", "Number Phone number can't be blank")
+      end
+      it "電話番号が11桁以内でないと登録できないこと" do
+        @user_item.number = "000000000000"
+        @user_item.valid?
+        expect(@user_item.errors.full_messages).to include("Number Phone number can't be blank")
+      end
+      it "電話番号が数字のみでないと登録できない" do
+        @user_item.number = "0901234567a"
+        @user_item.valid?
+        expect(@user_item.errors.full_messages).to include("Number Phone number can't be blank")
       end
       it "tokenが空では登録できないこと" do
         @user_item.token = nil
         @user_item.valid?
         expect(@user_item.errors.full_messages).to include("Token can't be blank")
+      end
+      it "user_idが空だと登録できない" do
+        @user_item.user_id = nil
+        @user_item.valid?
+        expect(@user_item.errors.full_messages).to include("User can't be blank")
+      end
+      it "item_idが空だと登録できない" do
+        @user_item.item_id = nil
+        @user_item.valid?
+        expect(@user_item.errors.full_messages).to include("Item can't be blank")
       end
     end
   end

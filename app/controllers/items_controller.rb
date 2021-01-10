2b38b6678e82fd6af412484143e2339c74470d36
sugastreet22 <re22sugar@gmail.com>
2021-01-10 18:46:20 +0900

    削除機能をできるように修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update]
-  before_action :sold_out_item, only: [:edit, :update, :destroy]
+  before_action :sold_out_item, only: [:edit, :update]
   def index
     @items = Item.all.order(created_at: :desc)
   end
